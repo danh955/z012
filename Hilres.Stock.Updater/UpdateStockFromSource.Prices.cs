c28cae789502d0de32dc6bb7e3cd6dab1cf0f9b1
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2021-06-15 19:25:57 +0900

    Added CreateUpdatePriceWorker

--- Hilres.Stock.Updater/UpdateStockFromSource.Prices.cs
+++ Hilres.Stock.Updater/UpdateStockFromSource.Prices.cs
@@ -16,29 +16,48 @@ namespace Hilres.Stock.Updater
     /// </summary>
     public partial class UpdateStockFromSource
     {
-        private async Task UpdateDataPrices(CancellationToken cancellationToken)
+        /// <summary>
+        /// Create an action block for updating prices.
+        /// </summary>
+        /// <param name="cancellationToken">CancellationToken.</param>
+        /// <returns>ActionBlock./returns>
+        private ActionBlock<int> CreateUpdatePriceWorker(CancellationToken cancellationToken)
         {
-            ActionBlock<int> updatePriceAction = new(
-                                this.PriceUpdateAction,
-                                new ExecutionDataflowBlockOptions()
-                                {
-                                    CancellationToken = cancellationToken,
-                                });
-
-            var stockIds = await this.GetStockIdsAsync(cancellationToken);
-            stockIds.ForEach(id => updatePriceAction.SendAsync(id, cancellationToken));
-
-            updatePriceAction.Complete();
-            await updatePriceAction.Completion;
+            return new(this.UpdatePriceWorkerAction,
+                        new ExecutionDataflowBlockOptions()
+                        {
+                            CancellationToken = cancellationToken,
+                        });
         }
 
-        private async Task<List<int>> GetStockIdsAsync(CancellationToken cancellationToken)
+        /// <summary>
+        /// This will update all the stock price.
+        /// </summary>
+        /// <param name="cancellationToken">CancellationToken.</param>
+        /// <returns>Task.</returns>
+        private async Task UpdateAllPrices(CancellationToken cancellationToken)
+        {
+            var stockIds = await this.GetAllStockIdsAsync(cancellationToken);
+            stockIds.ForEach(id => this.updatePriceWorker.SendAsync(id, cancellationToken));
+        }
+
+        /// <summary>
+        /// Get all the stock symbol IDs.
+        /// </summary>
+        /// <param name="cancellationToken">CancellationToken.</param>
+        /// <returns>Task.</returns>
+        private async Task<List<int>> GetAllStockIdsAsync(CancellationToken cancellationToken)
         {
             using var db = this.dbContextFactory.CreateDbContext();
             return await db.Stocks.Select(s => s.Id).Take(3).ToListAsync(cancellationToken);
         }
 
-        private async Task PriceUpdateAction(int stockId)
+        /// <summary>
+        /// Action block worker task that will update a single stock prices.
+        /// </summary>
+        /// <param name="stockId">Stock ID to update prices.</param>
+        /// <returns>Task.</returns>
+        private async Task UpdatePriceWorkerAction(int stockId)
         {
             this.logger.LogDebug("PriceUpdateAction({0})", stockId);
             await Task.Delay(1);
--- Hilres.Stock.Updater/UpdateStockFromSource.cs
+++ Hilres.Stock.Updater/UpdateStockFromSource.cs
@@ -5,9 +5,11 @@ namespace Hilres.Stock.Updater
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
     using Hilres.Stock.Repository;
     using Hilres.Stock.Updater.Abstraction;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
 
     /// <summary>
@@ -19,21 +21,27 @@ namespace Hilres.Stock.Updater
         private readonly IDbContextFactory<StockDbContext> dbContextFactory;
         private readonly IStockDataSource stockDataSource;
         private readonly ILogger logger;
+        private readonly ActionBlock<int> updatePriceWorker;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UpdateStockFromSource"/> class.
         /// </summary>
         /// <param name="logger">ILogger.</param>
+        /// <param name="appLifetime">IHostApplicationLifetime.</param>
         /// <param name="stockDataSource">IStockDataSource.</param>
         /// <param name="dbContextFactory">Database context factory for the stock.</param>
         public UpdateStockFromSource(
             ILogger<UpdateStockFromSource> logger,
+            IHostApplicationLifetime appLifetime,
             IStockDataSource stockDataSource,
             IDbContextFactory<StockDbContext> dbContextFactory)
         {
             this.logger = logger;
             this.stockDataSource = stockDataSource;
             this.dbContextFactory = dbContextFactory;
+
+            CancellationToken cancellationToken = appLifetime.ApplicationStopping;
+            this.updatePriceWorker = this.CreateUpdatePriceWorker(cancellationToken);
         }
 
         /// <summary>
@@ -44,7 +52,9 @@ namespace Hilres.Stock.Updater
         public async Task DoUpdate(CancellationToken cancellationToken)
         {
             await this.UpdateDataSymbols(cancellationToken);
-            await this.UpdateDataPrices(cancellationToken);
+            await this.UpdateAllPrices(cancellationToken);
         }
     }
+
+    //// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/web-host#ihostapplicationlifetime-interface
 }
